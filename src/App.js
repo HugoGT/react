11c2afa360b35c36555271502e1db5b02bf247de
Hugo <gthugo@outlook.com>
2022-12-29 20:37:26 -0500

    React hooks

--- src/App.js
+++ src/App.js
@@ -7,40 +7,38 @@ import { TodoSearch } from "./components/TodoSearch";
 import { TodoContext, TodoProvider } from "./components/TodoContext";
 
 function App() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    toggleCompleteTodo,
+    deleteTodo,
+  } = React.useContext(TodoContext);
+
   return (
     <TodoProvider>
       <React.Fragment>
         <TodoCounter />
         <TodoSearch />
 
-        <TodoContext.Consumer>
-          {({
-            error,
-            loading,
-            searchedTodos,
-            toggleCompleteTodo,
-            deleteTodo,
-          }) => (
-            <TodoList>
-              {error && <p>Desepérate, hubo un error</p>}
-              {loading && <p>Estamos cargando, no desesperes...</p>}
+        <TodoList>
+          {error && <p>Desepérate, hubo un error</p>}
+          {loading && <p>Estamos cargando, no desesperes...</p>}
 
-              {searchedTodos.map((todo) => (
-                <TodoItem
-                  key={todo.text}
-                  text={todo.text}
-                  completed={todo.completed}
-                  onComplete={() => {
-                    toggleCompleteTodo(todo.text);
-                  }}
-                  onDelete={() => {
-                    deleteTodo(todo.text);
-                  }}
-                />
-              ))}
-            </TodoList>
-          )}
-        </TodoContext.Consumer>
+          {searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => {
+                toggleCompleteTodo(todo.text);
+              }}
+              onDelete={() => {
+                deleteTodo(todo.text);
+              }}
+            />
+          ))}
+        </TodoList>
 
         <CreateTodoButton />
       </React.Fragment>
