0be769eea3ab797c5f993bba96ace8a9c955b103
Hugo <gthugo@outlook.com>
2022-12-31 18:42:02 -0500

    Fixed todo counter

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -3,7 +3,7 @@ import './css/TodoCounter.css';
 import { TodoContext } from './TodoContext';
 
 function TodoCounter() {
-  const { total: totalTodos, completed: completedTodos } = React.useContext(TodoContext);
+  const { totalTodos, completedTodos } = React.useContext(TodoContext);
 
   return (
     <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} TODOs</h2>
